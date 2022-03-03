4b8641416f66439786cfa99e1c73ec1de22a9cbd
flashnames <765719516@qq.com>
2022-03-04 00:54:12 +0800

    修改服务器代码 但是还未解决服务器端口问题

--- server-app/src/index.js
+++ server-app/src/index.js
@@ -1,39 +1,9 @@
-import path from "path";
-import express from "express"
-import { ReactDOMServer } from "react-dom/server";
-import Handle from "./server/AddressMessages/Handle";
-import fs from "fs"
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Handle from "./AddressMessages/Handle"
 
-const Port=process.env.Port || 3005;
-const app=(express);
-app.request(express.static("./AddressMessages/Handle.js"))
-
-app.get("/*",(req,res)=>{
-        const app=ReactDOMServer.renderToString(
-            <Handle />
-        );
-        const indexFile=path.resolve(
-            "./AddressMessages/Handle.js"
-        )
-        fs.readFile(indexFile,
-            "uft8",
-            (err,data)=>{
-                return res.send(
-                    data.replace(
-                        `<div id="root"></div>`,
-                        `<div id="root">${app}</div>`
-                    )
-                );
-            }
-        );
-    }
-);
-
-
-app.listen(
-    Port,
-    ()=>{
-    console.log(`Server is listening on port ${Port}`)
-    }
+ReactDOM.render(
+    <Handle />,
+    document.getElementById("root")
 )
